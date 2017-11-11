4eae239e2dbcb968eccd0068158357fbd7ba4802
m_sugimoto <m_sugimoto@sslab.ics.keio.ac.jp>
2017-11-11 12:00:13 +0000

    Set NCPU to 1 to ease debugging

--- param.h
+++ param.h
@@ -4,8 +4,9 @@
 #define NPROC        8  // maximum number of processes
 #define KSTACKSIZE 4096  // size of per-process kernel stack
 
-//add manabu 11/10
-#define NCPU          1  // maximum number of CPUs
+//add manabu 11/10`
+//#define NCPU          8  // maximum number of CPUs
+#define NCPU          1  // DEBUG
 //
 
 #define NOFILE       16  // open files per process
