2403ba55b27eb176bef895263efea95513d41783
m_sugimoto <m_sugimoto@sslab.ics.keio.ac.jp>
2017-12-10 14:03:49 +0000

    Set NCPU to 1 for time measurement

--- param.h
+++ param.h
@@ -5,8 +5,8 @@
 #define KSTACKSIZE 4096  // size of per-process kernel stack
 
 //add manabu 11/10`
-#define NCPU          8  // maximum number of CPUs
-//#define NCPU          1  // DEBUG
+//#define NCPU          8  // maximum number of CPUs
+#define NCPU          1  // DEBUG
 //
 
 #define NOFILE       16  // open files per process
